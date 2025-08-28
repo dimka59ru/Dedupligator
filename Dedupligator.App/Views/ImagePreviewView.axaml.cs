4c5bedef04d369d32a0c369db59bf31441c0df1c
Dmitriy Suvorov <rizan59ru@yandex.ru>
2025-08-28 19:11:31 +0500

    Вынес превтю картинки в отдельный контрол

---
+++ Dedupligator.App/Views/ImagePreviewView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Dedupligator.App.Views;
+
+public partial class ImagePreviewView : UserControl
+{
+    public ImagePreviewView()
+    {
+        InitializeComponent();
+    }
+}
