0f84139e29b648412876f6696f29b4d0b49d1110
Dmitriy Suvorov <rizan59ru@yandex.ru>
2025-09-17 19:59:15 +0500

    Различные оптимизации

---
+++ Dedupligator.Services/Factories/DuplicateMatchStrategyFactory.cs
@@ -0,0 +1,21 @@
+﻿using Dedupligator.Services.DuplicateFinders;
+using System.Collections.Concurrent;
+
+namespace Dedupligator.Services.Factories
+{
+  public class DuplicateMatchStrategyFactory : IDuplicateMatchStrategyFactory
+  {
+    private readonly ConcurrentDictionary<float, NeuralSimilarityStrategy> _neuralCache = new();
+    private readonly Lazy<ExactMatchStrategy> _exactMatchStrategy = new(() => new ExactMatchStrategy());
+    private readonly Lazy<SimilarImageStrategy> _similarImageStrategy = new(() => new SimilarImageStrategy());
+
+    public IDuplicateMatchStrategy CreateExactMatchStrategy() => _exactMatchStrategy.Value;
+
+    public IDuplicateMatchStrategy CreateSimilarImageStrategy() => _similarImageStrategy.Value;
+
+    public IDuplicateMatchStrategy CreateNeuralSimilarityStrategy(float threshold)
+    {
+      return _neuralCache.GetOrAdd(threshold, t => new NeuralSimilarityStrategy(t));
+    }
+  }
+}
